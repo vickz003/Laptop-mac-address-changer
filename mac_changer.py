12c5ab9a7fba6b155e24f7cec250dc7ac8df88a2
vignesh <62613745+vickz003@users.noreply.github.com>
2021-12-01 18:44:59 +0530

    First file mac changer

---
+++ mac_changer.py
@@ -0,0 +1,19 @@
+import subprocess
+import optparse
+
+a = optparse.OptionParser()
+
+a.add_option("-i","--interface",dest="interface",help="use to interface")
+a.add_option("-m","--mac",dest="mac",help="use to mac_address")
+
+(x , y) = a.parse_args()
+
+interface  = x.interface
+mac_address = x.mac
+
+subprocess.call("ifconfig " + interface + " down", shell=True)
+subprocess.call("ifconfig " + interface + " hw ether " + mac_address, shell=True)
+subprocess.call("ifconfig " + interface + " up", shell=True)
+
+print("changing " +interface+ " to " + mac_address + " ...........##@@##@")
+
